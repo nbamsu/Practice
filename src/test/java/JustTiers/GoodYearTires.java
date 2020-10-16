82562e7e9f1bd9cd7733258e7ef30b40ebffcdbb
Radle11 <62616139+Radle11@users.noreply.github.com>
2020-10-16 14:58:02 -0500

    added sout

--- src/test/java/JustTiers/GoodYearTires.java
+++ src/test/java/JustTiers/GoodYearTires.java
@@ -3,6 +3,8 @@ package JustTiers;
 public class GoodYearTires {
     public static void main(String[] args) {
         System.out.println("What do you want");
+        System.out.println("Nothing!!!");
+        System.out.println("Who Are YOU!!!");
     }
 
 }
